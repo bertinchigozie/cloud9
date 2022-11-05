b9f942cce85af64129ca19fc14a4ac4b72f51f61
bertinchigozie <bertinchigozie@gmail.com>
2022-11-05 22:45:24 +0100

    another commit 3

--- src/component/layouts/ContactButton.js
+++ src/component/layouts/ContactButton.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ContactButton.css";
 
 function ContactButton() {
   return (
     <div className="ContactButton">
       <p>
-        <a id="contact" rel="noreferrer" target="_blank" href="/contact">
-          Contact Me
-        </a>
+        <Link>
+          <a id="contact" rel="noreferrer" target="_blank" href="/contact">
+            Contact Me
+          </a>
+        </Link>
       </p>
     </div>
   );
